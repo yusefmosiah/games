e3ced891ed5b43346014cb11a5d32fdd9a8f1a13
Yusef Mosiah Nathanson <yusef@mosiah.org>
2023-02-13 13:20:16 -0500

    initalize mix project

---
+++ lib/games.ex
@@ -0,0 +1,18 @@
+defmodule Games do
+  @moduledoc """
+  Documentation for `Games`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Games.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
